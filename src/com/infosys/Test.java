5c63f8ab69f21eaed502aec9ad774c2d3834c5d0
Manoj k <manatharv@gmail.com>
2023-07-10 11:44:07 +0530

    pawan saved

---
+++ src/com/infosys/Test.java
@@ -0,0 +1,13 @@
+package com.infosys;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This project is created under the guidance of Pawan");
+		
+		
+
+	}
+
+}
