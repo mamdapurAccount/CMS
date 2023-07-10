f119f59dec1e7684d66f5bb84802dde0ca6b36d0
mamdapurAccount <56784976+mamdapurAccount@users.noreply.github.com>
2023-07-10 11:45:35 +0530

    Rishi has done changes

--- src/com/infosys/Test.java
+++ src/com/infosys/Test.java
@@ -4,7 +4,7 @@ public class Test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("This project is created under the guidance of Pawan");
+		System.out.println("This project is created under the guidance of rishi");
 		
 		
 
